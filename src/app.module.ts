410c617560029d98067f82160bff596d7394234d
louis <louis.duc@sourcode.com.au>
2023-10-06 00:21:18 +0700

    init project

---
+++ src/app.module.ts
@@ -0,0 +1,25 @@
+import { MikroORM } from '@mikro-orm/core';
+import { MikroOrmMiddleware, MikroOrmModule } from '@mikro-orm/nestjs';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  OnModuleInit,
+} from '@nestjs/common';
+
+@Module({
+  imports: [MikroOrmModule.forRoot()],
+  controllers: [],
+  providers: [],
+})
+export class AppModule implements NestModule, OnModuleInit {
+  constructor(private readonly orm: MikroORM) {}
+
+  async onModuleInit(): Promise<void> {
+    await this.orm.getMigrator().up();
+  }
+
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(MikroOrmMiddleware).forRoutes('*');
+  }
+}
