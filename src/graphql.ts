10cf1254fdd6227391607f2672dd186301181767
Glory Nguyen <glory.nguyen@sourcecode.com.au>
2023-10-06 01:17:15 +0700

    add generate graphql

---
+++ src/graphql.ts
@@ -0,0 +1,15 @@
+
+/*
+ * -------------------------------------------------------
+ * THIS FILE WAS AUTOMATICALLY GENERATED (DO NOT MODIFY)
+ * -------------------------------------------------------
+ */
+
+/* tslint:disable */
+/* eslint-disable */
+
+export interface IQuery {
+    stocks(): Nullable<number> | Promise<Nullable<number>>;
+}
+
+type Nullable<T> = T | null;
