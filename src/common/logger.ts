38e01c0f8b3fe8a82712dbca514ee63028dc0d99
tuna <tvmo@theamsgroup.com.au>
2023-10-06 16:37:29 +0700

    add log module

---
+++ src/common/logger.ts
@@ -0,0 +1,12 @@
+import { createLogger, transports, format } from 'winston';
+
+export default createLogger({
+  transports: [new transports.Console()],
+  format: format.combine(
+    format.colorize(),
+    format.timestamp(),
+    format.printf(({ timestamp, level, message }) => {
+      return `[${timestamp}] ${level}: ${message}`;
+    }),
+  ),
+});
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import logger from './common/logger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(process.env.EXPORT_PORT || 3000);
-  console.log(`Application is running on: ${await app.getUrl()}`);
+  logger.info(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
